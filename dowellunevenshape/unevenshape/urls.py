aec5eb64e68f94bfcc739a87d8da8eb405e17a8d
muchemicarol <cmcayo4@gmail.com>
2021-09-28 23:50:34 +0300

    app: include full pathname for dowellunevenshape view

--- dowellunevenshape/unevenshape/urls.py
+++ dowellunevenshape/unevenshape/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from unevenshape.views import find_shape_centre
+from unevenshape.views import index, find_shape_centre
 
 urlpatterns = [
-    # path('', index, name="index"),
-    path('', find_shape_centre, name="dowellunevenshape"),
+    path('', index, name="index"),
+    path('dowellunevenshape/', find_shape_centre, name="dowellunevenshape"),
 ]
 
